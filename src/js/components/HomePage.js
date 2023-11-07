a33e20ab622d1cbbdc841e0873214817f22ea1b8
Serhii Sapiannyk <serhii09@icloud.com>
2023-11-07 21:08:56 +0100

    add filtering by category HomePage

--- src/js/components/HomePage.js
+++ src/js/components/HomePage.js
@@ -1,6 +1,7 @@
 import SongList from './SongList.js';
-import { select} from './../settings.js';
+import {classNames, select} from './../settings.js';
 import MusicPlayer from './MusicPlayer.js';
+import utils from '../utils.js';
 
 class HomePage {
   constructor(songsData, categories) {
@@ -8,11 +9,12 @@ class HomePage {
 
     thisHomePage.data = {};
     thisHomePage.data.songs = songsData;
+    // console.log(thisHomePage.data.songs);
     thisHomePage.data.categories = categories;
 
     thisHomePage.getElements();
     thisHomePage.renderSongs();
-    // thisHomePage.filterByCategories();
+    thisHomePage.filterByCategories();
   }
 
   getElements() {
@@ -20,7 +22,9 @@ class HomePage {
 
     thisHomePage.dom = {};
     thisHomePage.dom.wrapper = document.querySelector(select.containerOf.homePage);
+    // console.log(thisHomePage.dom.wrapper);
     thisHomePage.dom.categoryList = document.querySelector(select.listOf.categories);
+    // console.log(thisHomePage.dom.categoryList);
     thisHomePage.dom.categoryLinks = document.querySelectorAll(select.linksOf.categories);
   }
 
@@ -31,6 +35,57 @@ class HomePage {
       new SongList(thisHomePage.data.songs[songData], thisHomePage.dom.wrapper);
     }
     thisHomePage.initWidgets();
+  }
+
+  filterByCategories() {
+    const thisHomePage = this;
+
+    let activeCategory = '';
+
+
+    thisHomePage.dom.categoryList.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      utils.resetWrapper(thisHomePage.dom.wrapper);
+
+      const category = event.target;
+
+
+      if(category.classList.contains(classNames.categories.isCategory)) {
+        const categoryName = category.getAttribute(select.attributesOf.category);
+
+        if (!category.classList.contains(classNames.categories.active)) {
+          thisHomePage.resetCategories();
+          category.classList.add(classNames.categories.active);
+          activeCategory = categoryName;
+          // console.log(activeCategory);
+
+          for (let songData in thisHomePage.data.songs) {
+            const songCategories = thisHomePage.data.songs[songData].categories;
+            // console.log(songCategories);
+          
+            if (songCategories.includes(activeCategory)) {
+              new SongList(thisHomePage.data.songs[songData], thisHomePage.dom.wrapper);
+            }
+          }
+          thisHomePage.initWidgets();
+        } else {
+          category.classList.remove(classNames.categories.active);
+          activeCategory = '';
+          thisHomePage.renderSongs();
+        }
+      }
+    });
+  }
+
+  resetCategories() {
+    const thisHomePage = this;
+
+    const allCategoryLinks = thisHomePage.dom.categoryLinks;
+
+    for (let categoryLink of allCategoryLinks) {
+      categoryLink.classList.remove(classNames.categories.active);
+    }
   }
 
   initWidgets() {
